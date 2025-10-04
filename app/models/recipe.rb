392d4e66d13ac1612a26eb283159efc994be5135
Pedro Ariztia <ppariztia1@miuandes.cl>
2025-10-04 15:48:31 -0300

    despues sigo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
-  validates :title :instructions, presence: true
+  validates :title, presence: true
+  validates :instructions, presence: true
+  validates :cook_time, numericality: { only_integer: true, greater_than: 0 }
+  
   has_rich_text :instructions
 end
