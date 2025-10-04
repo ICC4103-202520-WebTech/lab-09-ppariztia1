1b1207468710095c2685ca9c86adaa9f561ec204
Pedro Ariztia <ppariztia1@miuandes.cl>
2025-10-04 15:18:37 -0300

    views listas

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,50 @@
+class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @recipes = Recipe.all
+    end
+
+    def show
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        recipe = Recipe.new recipe_params
+        if recipe.save
+            redirect_to recipe_path(recipe)
+        else
+            redirect_to new_recipe_path
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @recipe.update recipe_params
+            redirect_to recipe_path(@recipe)
+        else
+            redirect_to edit_recipe_path(@recipe)
+        end
+    end
+
+    def destroy
+        @recipe.destroy
+        redirect_to recipes_path
+    end
+
+
+    private
+    #Strong parameters
+    def recipe_params
+        params.require(:recipe).permit(:name, :ingredients, :instructions)
+    end
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+end
